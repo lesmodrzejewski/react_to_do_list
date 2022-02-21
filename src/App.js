c71d3e9c4a208cd88483c0b16bcc943e48253788
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-02-21 20:11:08 +0000

    Extensions completed

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [allTasks, setAllTasks] = useState(["Shopping", "Exercising", "Coding"])
-  const [newTask, setTask] = useState("")
+  const [allTasks, setAllTasks] = useState([
+    {name: 'Shopping', priority: 'low'},
+    {name: 'Coding', priority: 'high'},
+    {name: 'Cleaning Car', priority: 'low'},
+    {name: 'Cooking', priority: 'high'}
+  ])
+
+  const [newTask, setTask] = useState('')
+  const [taskPriority, setTaskPriority] = useState('')
 
   const showTasks = allTasks.map((task, index) => {
-    return <li key={index}>{task}</li>
+    console.log(task);
+    return <li key={index} className={task.priority}>{task.name}</li>
   })
 
   const handleTaskInput = (event) => {
-    console.log('change');
     setTask(event.target.value)
   }
 
   const saveNewTask = (event) => {
     event.preventDefault();
-    console.log('newTask')
-    const newTaskItem = newTask
+    const newTaskItem = {name: newTask, priority: taskPriority}
     const newListOfTasks = [...allTasks, newTaskItem];
     setAllTasks(newListOfTasks);
     setTask('')
   }
 
-
-
+  const handleChange = (event) => {
+    setTaskPriority(event.target.value)
+  }
 
   return (
     <div className='to-do-list-container'>
 
       <h1>To Do List</h1>    
       <hr />  
+
+      <h2 className='high'>high priority</h2>
+      <h2 className='low'>low priority</h2>
 
       <ul>
         {showTasks}
@@ -41,6 +51,10 @@ function App() {
       <form onSubmit={saveNewTask}>
         <label htmlFor="to-do-task-input">Add a new to do task:</label>
         <input type="text" name="to-do-task-input" id="to-do-task-input" value={newTask} onChange={handleTaskInput}/>
+        <label htmlFor="task-low">Low</label>
+        <input type="radio" name="priority" id="task-low" value="low" onChange={handleChange} defaultChecked/>
+        <label htmlFor="task-high">High</label>
+        <input type="radio" name="priority" id="task-high" value="high" onChange={handleChange}/>
         <input type="submit" value="Save new task" />
       </form>
 
